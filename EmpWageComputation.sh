e9a5a2e608a008b8c4dcf4830cd4dc4c921a58e6
lavanya <lavanyace929@gmail.com>
2021-01-11 21:26:58 +0530

    Added:Calculating daily wage

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,11 +1,20 @@
-#Welcome to the employee wage computation problem statement
+
 #!/bin/bash -x
+
+#constants
 IS_PRESENT=1
+EMP_RATE_PER_HR=20
+FULL_TIME=8
+
+#variables
 EmpCheck=$(( RANDOM%2 ))
+DailyWage=0
 
 if [ $EmpCheck ==  $IS_PRESENT ]
 then
 	echo "Employee is present"
+	DailyWage=$(($EMP_RATE_PER_HR*$FULL_TIME))
+	echo $DailyWage	
 else 
 	echo "Employee is abscent"
 fi
