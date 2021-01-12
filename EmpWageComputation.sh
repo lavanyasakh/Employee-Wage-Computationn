1d732af51b8f78de6a5a5154ae53fbba19bd4779
lavanya <lavanyace929@gmail.com>
2021-01-12 12:43:58 +0530

    Added:Monthly wage in switch case

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -5,22 +5,20 @@ IS_FULL_TIME=1
 EMP_RATE_PER_HR=20
 IS_PART_TIME=2
 
-
-#variables
-EmpCheck=$(( RANDOM%3  ))
-DailyWage=0
-
-case $EmpCheck in
-	$IS_FULL_TIME)
-		EmpHrs=8
+for (( i=1; i<=20; i++ ))
+do
+	EmpCheck=$(( RANDOM%3  ))
+	case $EmpCheck in
+		$IS_FULL_TIME)
+			EmpHrs=8
 			;;
-	$IS_PART_TIME)
-		EmpHrs=4
+		$IS_PART_TIME)
+			EmpHrs=4
 			;;
-	*)
-		EmpHrs=0
+		*)
+			EmpHrs=0
 		   ;;
 esac
-
-
-DailyWage=$(($EMP_RATE_PER_HR*$EmpHrs))
+Salary=$(($EMP_RATE_PER_HR*$EmpHrs))
+Totalsalary=$((Totalsalary+Salary))
+done
