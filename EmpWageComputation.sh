f6609b5e8fcd17b0ee6d9842469e564c8b1a75a2
lavanya <lavanyace929@gmail.com>
2021-01-12 13:15:43 +0530

    Added:Employee wage on a condition in switch case

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -4,16 +4,25 @@
 IS_FULL_TIME=1
 EMP_RATE_PER_HR=20
 IS_PART_TIME=2
+MAX_WORK_HRS_IN_MONTH=100
+MAX_NO_OF_WORKING_DAYS=20
 
-for (( i=1; i<=20; i++ ))
+#variables
+WorkHrs=0
+WorkDays=0
+
+
+while [ $WorkDays -lt $MAX_NO_OF_WORKING_DAYS  -a  $WorkHrs -lt $MAX_WORK_HRS_IN_MONTH  ]
 do
 	EmpCheck=$(( RANDOM%3  ))
 	case $EmpCheck in
 		$IS_FULL_TIME)
 			EmpHrs=8
+			((WorkDays++))	
 			;;
 		$IS_PART_TIME)
 			EmpHrs=4
+			((WorkDays++))
 			;;
 		*)
 			EmpHrs=0
@@ -21,4 +30,5 @@ do
 esac
 Salary=$(($EMP_RATE_PER_HR*$EmpHrs))
 Totalsalary=$((Totalsalary+Salary))
+WorkHrs=$(($WorkHrs+$EmpHrs))
 done
