0f9332c2e3d7f853c448cf3f5f6dd82c8e804425
lavanya <lavanyace929@gmail.com>
2021-01-12 11:49:39 +0530

    Added:DailyWageWithSwitchCase

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -2,23 +2,25 @@
 
 #constants
 IS_FULL_TIME=1
-FULL_TIME_HRS=8
 EMP_RATE_PER_HR=20
 IS_PART_TIME=2
-PART_TIME_HRS=4
+
 
 #variables
 EmpCheck=$(( RANDOM%3  ))
 DailyWage=0
-if [ $EmpCheck == $IS_FULL_TIME ]
-then
-	echo " Employee is full time"
-	DailyWage=$(($EMP_RATE_PER_HR*$FULL_TIME_HRS))
-elif [ $EmpCheck == $IS_PART_TIME ]
-then
-	echo "Employee is part time"
-	DailyWage=$(($EMP_RATE_PER_HR*$PART_TIME_HRS))
-else 
-	echo "Employee is abscent"
-fi
 
+case $EmpCheck in
+	$IS_FULL_TIME)
+		EmpHrs=8
+			;;
+	$IS_PART_TIME)
+		EmpHrs=4
+			;;
+	*)
+		EmpHrs=0
+		   ;;
+esac
+
+
+DailyWage=$(($EMP_RATE_PER_HR*$EmpHrs))
