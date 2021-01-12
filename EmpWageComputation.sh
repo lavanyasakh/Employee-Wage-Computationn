8d52a943b8161b1f783b7c8bfc48f76c8e4cdddc
lavanya <lavanyace929@gmail.com>
2021-01-12 11:12:37 +0530

    Added:FullAndPartTimeDailyWage

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,20 +1,23 @@
-
 #!/bin/bash -x
 
 #constants
-IS_PRESENT=1
+IS_FULL_TIME=1
+FULL_TIME_HRS=8
 EMP_RATE_PER_HR=20
-FULL_TIME=8
+IS_PART_TIME=2
+PART_TIME_HRS=4
 
 #variables
-EmpCheck=$(( RANDOM%2 ))
+EmpCheck=$(( RANDOM%3  ))
 DailyWage=0
-
-if [ $EmpCheck ==  $IS_PRESENT ]
+if [ $EmpCheck == $IS_FULL_TIME ]
 then
-	echo "Employee is present"
-	DailyWage=$(($EMP_RATE_PER_HR*$FULL_TIME))
-	echo $DailyWage	
+	echo " Employee is full time"
+	DailyWage=$(($EMP_RATE_PER_HR*$FULL_TIME_HRS))
+elif [ $EmpCheck == $IS_PART_TIME ]
+then
+	echo "Employee is part time"
+	DailyWage=$(($EMP_RATE_PER_HR*$PART_TIME_HRS))
 else 
 	echo "Employee is abscent"
 fi
