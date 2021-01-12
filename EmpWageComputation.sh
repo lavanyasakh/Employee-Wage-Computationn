07d036ca98e12526a1eb94b0f52eee27ef884f39
lavanya <lavanyace929@gmail.com>
2021-01-12 14:16:44 +0530

    Ref:Calculate work hrs in Function

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -11,24 +11,29 @@ MAX_NO_OF_WORKING_DAYS=20
 WorkHrs=0
 WorkDays=0
 
+function GetWorkHrs() {
+	local EmpCheck=$1
+   case $EmpCheck in
+      $IS_FULL_TIME)
+         EmpHrs=8	
+         ;;
+      $IS_PART_TIME)
+         EmpHrs=4
+         ;;
+      *)
+         EmpHrs=0
+         ;;
+	esac
+	echo $EmpHrs
+}
+		
+
 
 while [ $WorkDays -lt $MAX_NO_OF_WORKING_DAYS  -a  $WorkHrs -lt $MAX_WORK_HRS_IN_MONTH  ]
 do
+	((WorkDays++))
 	EmpCheck=$(( RANDOM%3  ))
-	case $EmpCheck in
-		$IS_FULL_TIME)
-			EmpHrs=8
-			((WorkDays++))	
-			;;
-		$IS_PART_TIME)
-			EmpHrs=4
-			((WorkDays++))
-			;;
-		*)
-			EmpHrs=0
-		   ;;
-esac
-Salary=$(($EMP_RATE_PER_HR*$EmpHrs))
-Totalsalary=$((Totalsalary+Salary))
-WorkHrs=$(($WorkHrs+$EmpHrs))
+	EmpHrs="$(GetWorkHrs  $EmpCheck)"
+	WorkHrs=$(($WorkHrs+$EmpHrs))
 done
+TotalSalary=$(( $EMP_RATE_PER_HR*$WorkHrs ))
